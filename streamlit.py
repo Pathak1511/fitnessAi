6759bfbe237ed6a79af15b7dba7b1850b69cd3f4
Pathak1511 <hritik1511@gmail.com>
2023-02-22 22:29:10 +0530

    modified

--- streamlit.py
+++ streamlit.py
@@ -45,7 +45,7 @@ RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
-frame = webrtc_streamer(key="TEST",
+frame = webrtc_streamer(
     key="WYH",
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTC_CONFIGURATION,
