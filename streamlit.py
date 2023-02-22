e90587827bc18671508df9545a8d106208f2a1e6
Pathak1511 <hritik1511@gmail.com>
2023-02-22 21:51:16 +0530

    modified

--- streamlit.py
+++ streamlit.py
@@ -44,6 +44,19 @@ st.metric(label="Calories Burned", value="0", delta="1")
 if st.button("Stop exercise."):
   st.balloons()
   st.success("You did it!")
+st.session_state['answer'] = ''!
+
+st.write(st.session_state)
+
+realans = ['', 'abc', 'edf']
+
+if  st.session_state['answer'] in realans:
+    answerStat = "correct"
+elif st.session_state['answer'] not in realans:
+    answerStat = "incorrect"
+
+st.write(st.session_state)
+st.write(answerStat)
   
 
   
